fdaab2dd9baf97589599154fc50233262eb8aa33
[gobbleveda] <[gobbleveda@yahoo.com]>
2020-02-18 20:02:46 -0600

    changed run time parameters

--- src/test/scala/BenchmarkAllGetAPIs.scala
+++ src/test/scala/BenchmarkAllGetAPIs.scala
@@ -103,7 +103,7 @@ class BenchmarkAllGetAPIs extends Simulation {
     scn.inject(
       nothingFor(2),
       atOnceUsers(5),
-      rampUsers(100) during( 60 seconds)
+      rampUsers(10) during( 60 seconds)
     ).protocols(httpProtocol)
   )
 
