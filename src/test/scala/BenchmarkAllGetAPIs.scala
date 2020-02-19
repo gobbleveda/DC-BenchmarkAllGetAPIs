9398f6593d428c733ee03389725d8a3241c34116
[gobbleveda] <[gobbleveda@yahoo.com]>
2020-02-19 10:57:48 -0600

    changed baseURL amanmisra.io

--- src/test/scala/BenchmarkAllGetAPIs.scala
+++ src/test/scala/BenchmarkAllGetAPIs.scala
@@ -11,7 +11,7 @@ class BenchmarkAllGetAPIs extends Simulation {
 
   // 1 configure http base url
 
-  val httpProtocol = http.baseUrl("http://0.0.0.0:5001")
+  val httpProtocol = http.baseUrl("http://amanmisra.io")
                          .header("Content-Type","application/json")
 
   //val csvFeeder = csv("/Users/amanmisra/Projects/Tutorials/PerformanceEngineering/gatling/DCGetAPIs/src/test/resources/data/new-user-registration-devcamp.csv").circular
@@ -102,8 +102,8 @@ class BenchmarkAllGetAPIs extends Simulation {
   setUp(
     scn.inject(
       nothingFor(2),
-      atOnceUsers(5),
-      rampUsers(45) during (45 seconds)
+      atOnceUsers(1),
+      rampUsers(1) during (1 seconds)
     ).protocols(httpProtocol)
   )
 
